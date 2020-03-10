322d9433b11ad8afd0965e676f97fc72fafee4a7
Bradulov Igor Vitalie <igor.bradulov@asp.gov.md>
2020-03-10 15:18:33 +0200

    изменена структура проекта

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 
 import {createStore} from 'redux';
-const reducer = (state=0,action)=>{
-  switch(action.type){
-    case 'INC': return state+1;
-    case 'DEC':return state-1;
-    case 'RND':return state+action.payload;
-    default:return state;
-  }
-};
+import {inc,dec,rnd} from './actions';
+import {reducer} from './reducer';
+
 
 const store = createStore(reducer);
-
-const inc =()=>({type:'INC'});
-const dec =()=>({type:'DEC'});
-const rnd =(payload)=>({type:'RND',payload});
-
 
 document.getElementById('inc').addEventListener('click',function(){
 	store.dispatch(inc());
