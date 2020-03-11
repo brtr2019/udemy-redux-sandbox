2837ec62a6a2ad2e23305feabcb5283f3fb714fc
Bradulov Igor Vitalie <igor.bradulov@asp.gov.md>
2020-03-11 15:07:49 +0200

    Использование React и Redux

--- src/counter.js
+++ src/counter.js
@@ -1,12 +1,15 @@
 import React from 'react';
-const Counter =()=>
+const Counter =({counter,inc,dec,rnd})=>
 {
+
+
+
 	return (
-		<div class="jumbotron">
-			<h2 id="counter">0</h2>
-	    	<button id="inc" className="btn-primary btn-lg">INC</button>
-	    	<button id="dec" className="btn-primary btn-lg">DEC</button>
-	    	<button id="rnd" className="btn-primary btn-lg">RND</button>	
+		<div className="jumbotron">
+			<h2 id="counter">{counter}</h2>
+	    	<button id="inc" className="btn-primary btn-lg" onClick={inc}>INC</button>
+	    	<button id="dec" className="btn-primary btn-lg" onClick={dec}>DEC</button>
+	    	<button id="rnd" className="btn-primary btn-lg" onClick={rnd}>RND</button>	
 		</div>
 	)	
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Counter from './counter';
 import {createStore,bindActionCreators} from 'redux';
+import Counter from './counter';
 import * as actions from './actions';
 import {reducer} from './reducer';
 
@@ -15,9 +14,19 @@ const {dispatch} = store;
 const {inc,dec,rnd} = bindActionCreators(actions,dispatch);
 
 
+
 const update = ()=>{
+	ReactDOM.render(<Counter counter={store.getState()} 
+						 inc={inc} 
+						 dec={dec} 
+						 rnd={()=>{
+						 	const value = Math.floor(Math.random()*10); 
+						 	rnd(value); 
+						 }
+} />,document.getElementById('root'));
 }
 
+update();
 store.subscribe(update);  
 
-ReactDOM.render(<Counter/>,document.getElementById('root'));
+
